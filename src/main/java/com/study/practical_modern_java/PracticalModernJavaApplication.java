c1c1dbf8dbc7bb9d129bfdd85a4e1d43bbf338e8
93hong <sk2rldnr@gmail.com>
2021-03-27 11:29:06 +0900

    initial commit

---
+++ src/main/java/com/study/practical_modern_java/PracticalModernJavaApplication.java
@@ -0,0 +1,13 @@
+package com.study.practical_modern_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalModernJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalModernJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/practical_modern_java/PracticalModernJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.practical_modern_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalModernJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
